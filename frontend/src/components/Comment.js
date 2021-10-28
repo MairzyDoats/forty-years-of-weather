80bb2564a8eea82f58f1cfad9434393b17959e6a
MairzyDoats <jk101@gmx.de>
2021-10-28 17:57:59 +0200

    add comment

---
+++ frontend/src/components/Comment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/comment.css'
+
+export default function Comment() {
+    return (
+        <div>
+        	<h2 className="comment__heading">A comment on climate change</h2>
+    			<p className="comment__text">Weather is immediate and kind of chaotic, but climate change and global warming are very much a real thing. At times the temperature over the course of 40 years might not have changed a lot and that does not debunk science. There is a scientific consensus among numerous experts about climate change and its human cause. Drastic measures are necessary, this little website does not change that.</p>
+        </div>
+    )
+}
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,11 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Chart from './Chart';
+import Comment from './Comment';
 
 export default function Main() {
 
+  const hasFetchedData = useRef(false)
+  const [weatherDates, setWeatherDates] = useState([])
+
+  useEffect(() => {
+    if (!hasFetchedData.current) {
+      fetch(process.env.REACT_APP_BACKEND_API_ENDPOINT)
+        .then(response => response.json())
+        .then(data => setWeatherDates(data))
+      hasFetchedData.current = true;
+    }
+  }, [])
+
   return (
     <main className="main">
-      <Chart />
+      <Chart 
+      data={weatherDates}
+      />
+      <Comment />
     </main>
   )
 }
