016c879ad8e04551c3550ed4b77da0c15b1a98f3
MairzyDoats <jk101@gmx.de>
2021-10-29 16:39:24 +0200

    change chart style

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -6,29 +6,33 @@ export default function Chart({ data }) {
   const d3LineChart = useRef()
   console.log(data);
 
+  
+
   useEffect(() => {
     const temperaturesByDate = []
+    const temperatures = []
     data.forEach((item) => {
-      if (item === null) {
-        return
-      }
+      if (item) {
       var dt = new Date(parseInt(item.dt.$date.$numberLong))
       var temp = parseFloat(item.main.temp.$numberDouble)
       if (!item.main.temp.$numberDouble)
         temp = parseFloat(item.main.temp.$numberInt)
-      
+      temperatures.push(temp)
       temperaturesByDate.push({date: dt, temperature: temp})
+      }
     })
     console.log(temperaturesByDate);
 
     const margin = {top: 20, right: 30, bottom: 30, left: 30}
-    const width = parseInt(d3.select('#d3demo').style('width'))
-    const height = parseInt(d3.select('#d3demo').style('height'))
+    const width = parseInt(d3.select("#d3demo").style("width"))
+    const height = parseInt(d3.select("#d3demo").style("height"))
+
+    console.log(height);
 
     // create svg element:
     const svg = d3.select(d3LineChart.current)
     .attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
+		.attr('height', height + margin.top + margin.bottom + 40)
     .append('g')
       .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
@@ -41,22 +45,37 @@ export default function Chart({ data }) {
     .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(x))
 
-    // Get the max value of counts
-    const max = d3.max(temperaturesByDate, function(d){return d.temperature})
+    // Default max and min temperatures with the option to 
+    const minTemp = Math.min(0, Math.floor(d3.min(temperatures)));
+    const maxTemp = Math.max(35, Math.ceil(d3.max(temperatures)))
 
     // y axis scale 
     const y = d3.scaleLinear()
-          .domain([0, max])
-          .range([height,0])
+          .domain([minTemp, maxTemp])
+          .range([height, 0])
   
     svg.append('g')
       .attr('class', 'chart__axes')
       .call(d3.axisLeft(y))
-  
-    // Add the path using this helper function
+
+    // x axis label
+    svg.append("text")
+      .attr("class", "chart__label")
+      .attr("text-anchor", "end")
+      .attr("x", width)
+      .attr("y", height -6)
+      .text("Years");
+    
+      // y axis label
+    svg.append("text")
+      .attr("class", "chart__label")
+      .attr("x", - 15)
+      .attr("y", - 5)
+      .text("°C");
+
     svg.append('path')
       .datum(temperaturesByDate)
-      .attr('class', 'chart__stroke')
+      .attr('class', 'chart__line')
       .attr('d', d3.line()
                           .x(function(d) { return x(d.date) })
                           .y(function(d) { return y(d.temperature) }));
@@ -65,7 +84,7 @@ export default function Chart({ data }) {
 
   return (
     <div id="d3demo">
-      <svg 
+      <svg
         ref={d3LineChart}
         ></svg>
     </div>
