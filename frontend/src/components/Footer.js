7f5a6b1710eb7ee266742494fe795be6b0f3df06
MairzyDoats <jk101@gmx.de>
2021-11-04 23:25:50 +0100

    Add copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,13 @@ import React from 'react'
 import "../css/footer.css"
 
 export default function Footer() {
+  const date = new Date()
+
   return (
     <div className="footer">
-      <a href="/">Imprint</a>
+      <a className="footer__link" href="/">Imprint</a>
+      <p className="footer__copyright">&copy;{date.getFullYear()} Julian Krüger</p>
     </div>
+
   )
 }
