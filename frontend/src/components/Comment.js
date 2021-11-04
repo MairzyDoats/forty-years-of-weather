d921052a992d9256e0e85765def21a3766b58a92
MairzyDoats <jk101@gmx.de>
2021-11-04 23:25:26 +0100

    Add collapsing text

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/comment.css'
 
 export default function Comment({ heading, text }) {
-    return (
-        <div className="comment">
-        	<h2 className="comment__heading">{heading}</h2>
-    			<p className="comment__text">{text}</p>
-        </div>
-    )
+	const [textToggle, setTextToggle] = useState(false);
+
+	return (
+		<div className="comment">
+			<div>
+				<h2 className="comment__heading">{heading} <button onClick={() => { setTextToggle(prevToggle => !prevToggle) }} className={textToggle ? "comment__btn comment__btn--toggled" : "comment__btn"}>&gt;</button></h2>
+			</div>
+			<p className={textToggle ? "comment__text" : "comment__text comment__text--invisible"}>{text}</p>
+		</div>
+	)
 }
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -18,18 +18,18 @@ export default function Main() {
 
   return (
     <main className="main">
-      <Chart 
-      data={weatherDates}
+      <Chart
+        data={weatherDates}
       />
-      <Comment 
+      <Comment
         heading="About 40 Years Of Weather"
         text="Plain and simple: Today's temperature in the city of Hamburg, Germany at this hour, over the course of the last forty years."
       />
-      <Comment 
+      <Comment
         heading="How is it done?"
         text="Not as plain and simple. To save costs the data came from the Open Weather Map API but only as a download of bulk data that was done once. That data was pushed into a MongoDB collection. To keep the data updated a Github Actions script runs hourly and saves the current temperature to the database using said Open Weather Map API. A MongoDB Webhook serves as an API to pass the 40 Years of Data to the backend of this React Web Application. The visualization is done with the help of d3."
       />
-      <Comment 
+      <Comment
         heading="A comment on climate change"
         text="Weather is immediate and kind of chaotic, but climate change and global warming are very much a real thing. At times the temperature over the course of 40 years might not have changed a lot and that does not debunk science. There is a scientific consensus among numerous experts about climate change and its human cause. Drastic measures are necessary, this little website does not change that."
       />
