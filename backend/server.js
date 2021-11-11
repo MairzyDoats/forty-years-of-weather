f61aeb410c4d8fdff7548e6d43375140ee291915
MairzyDoats <jk101@gmx.de>
2021-11-12 00:27:58 +0100

    update port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const router = require("./api/dates.route.js");
 const dotenv = require("dotenv");
 
 dotenv.config()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
